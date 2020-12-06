9c2df6512c71df9516697d6851331762c1e162b9
adnanashrafdevtek <adnan.ashraf@devtek.ai>
2020-12-05 21:32:41 -0600

    added eventing rest APIs and data model

---
+++ src/main/java/com/alerting/domain/Event.java
@@ -0,0 +1,55 @@
+
+package com.alerting.domain;
+
+
+import java.time.Instant;
+import java.util.ArrayList;
+import java.util.List;
+
+public class Event {
+    private int id;
+    private String object;
+    private Instant date;
+    private String action;
+    private List<Change> changes = new ArrayList<Change>();
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getObject() {
+        return object;
+    }
+
+    public void setObject(String object) {
+        this.object = object;
+    }
+
+    public Instant getDate() {
+        return date;
+    }
+
+    public void setDate(Instant date) {
+        this.date = date;
+    }
+
+    public String getAction() {
+        return action;
+    }
+
+    public void setAction(String action) {
+        this.action = action;
+    }
+
+    public List<Change> getChanges() {
+        return changes;
+    }
+
+    public void setChanges(List<Change> changes) {
+        this.changes = changes;
+    }
+}
