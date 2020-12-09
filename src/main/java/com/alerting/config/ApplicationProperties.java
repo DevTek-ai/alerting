8151c76e54be1d1698bcd89083fc54e7da7a2534
mirzadevtek <70073170+mirzadevtek@users.noreply.github.com>
2020-12-10 03:33:57 +0800

    new changes

---
+++ src/main/java/com/alerting/config/ApplicationProperties.java
@@ -0,0 +1,13 @@
+package com.alerting.config;
+
+import org.springframework.boot.context.properties.ConfigurationProperties;
+
+/**
+ * Properties specific to Alerting.
+ * <p>
+ * Properties are configured in the {@code application.yml} file.
+ * See {@link io.github.jhipster.config.JHipsterProperties} for a good example.
+ */
+@ConfigurationProperties(prefix = "application", ignoreUnknownFields = false)
+public class ApplicationProperties {
+}
---
+++ src/main/java/com/alerting/config/audit/package-info.java
@@ -0,0 +1,4 @@
+/**
+ * Audit specific code.
+ */
+package com.alerting.config.audit;
