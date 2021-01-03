920190454df8332449fcad778218c2a305aaea5b
mirzadevtek <70073170+mirzadevtek@users.noreply.github.com>
2021-01-04 04:06:33 +0800

    .

--- src/main/resources/config/liquibase/changelog/release1.3.alter.sql
+++ src/main/resources/config/liquibase/changelog/release1.3.alter.sql
@@ -11,9 +11,9 @@ DROP COLUMN `attribute`,
 DROP COLUMN `type`,
 DROP COLUMN `condition`,
 ADD COLUMN `type_selection` VARCHAR(255) NULL AFTER `query_statement`,
-ADD COLUMN `attribute_selection` VARCHAR(255) NULL AFTER `type_selection`;
+ADD COLUMN `attribute_selection` VARCHAR(255) NULL AFTER `type_selection`,
 ADD COLUMN `behaviour_selection` VARCHAR(255) NULL AFTER `attribute_selection`,
-ADD COLUMN `condition_selection` VARCHAR(255) NULL AFTER `behaviour_selection`;
+ADD COLUMN `condition_selection` VARCHAR(255) NULL AFTER `behaviour_selection`,
 ADD COLUMN `custom_attribute_selection` VARCHAR(255) NULL AFTER `condition_selection`,
 ADD COLUMN `from_date` DATETIME NULL AFTER `custom_attribute_selection`,
 ADD COLUMN `to_date` DATETIME NULL AFTER `from_date`;
