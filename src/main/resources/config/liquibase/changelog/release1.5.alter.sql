679d7042da2e264728df749f3a4fd48c2b663d44
mirzadevtek <70073170+mirzadevtek@users.noreply.github.com>
2021-01-06 14:17:24 +0800

    fixed the db changes

---
+++ src/main/resources/config/liquibase/changelog/release1.5.alter.sql
@@ -0,0 +1,42 @@
+--liquibase formatted sql
+
+--changeset mirza:release1.5.alters.sql
+
+-- -----------------------------------------------------
+-- Table `alerting`.`alert_definition`
+-- -----------------------------------------------------
+SET FOREIGN_KEY_CHECKS=0;
+DROP TABLE IF EXISTS `alert_definition` ;
+SET FOREIGN_KEY_CHECKS=1;
+CREATE TABLE IF NOT EXISTS `alert_definition` (
+  `id` BIGINT(20) NOT NULL AUTO_INCREMENT,
+  `title` VARCHAR(255) NULL DEFAULT NULL,
+  `message` VARCHAR(255) NULL DEFAULT NULL,
+  `trigger_type` VARCHAR(255) NULL DEFAULT NULL,
+  `notify` BIT(1) NULL DEFAULT NULL,
+  `category` VARCHAR(255) NULL DEFAULT NULL,
+  `date_created` DATETIME NULL DEFAULT NULL,
+  `date_updated` DATETIME NULL DEFAULT NULL,
+  `schedular_id` BIGINT(20) NULL DEFAULT NULL,
+  `statement_id` BIGINT(20) NULL DEFAULT NULL,
+  `query_statement` VARCHAR(256) NULL DEFAULT NULL,
+  `type_selection` VARCHAR(255) NULL DEFAULT NULL,
+  `attribute_selection` VARCHAR(255) NULL DEFAULT NULL,
+  `behaviour_selection` VARCHAR(255) NULL DEFAULT NULL,
+  `condition_selection` VARCHAR(255) NULL DEFAULT NULL,
+  `custom_attribute_selection` VARCHAR(255) NULL DEFAULT NULL,
+  `from_date` DATETIME NULL DEFAULT NULL,
+  `to_date` DATETIME NULL DEFAULT NULL,
+  PRIMARY KEY (`id`),
+  UNIQUE INDEX `ux_alert_definition_statement_id` (`statement_id` ASC) VISIBLE,
+  UNIQUE INDEX `ux_alert_definition_schedular_id` (`schedular_id` ASC) VISIBLE,
+  CONSTRAINT `fk_alert_definition_schedular_id`
+    FOREIGN KEY (`schedular_id`)
+    REFERENCES `alerting`.`schedular` (`id`),
+  CONSTRAINT `fk_alert_definition_statement_id`
+    FOREIGN KEY (`statement_id`)
+    REFERENCES `alerting`.`statement` (`id`))
+ENGINE = InnoDB
+AUTO_INCREMENT = 11
+DEFAULT CHARACTER SET = utf8mb4
+COLLATE = utf8mb4_0900_ai_ci;
