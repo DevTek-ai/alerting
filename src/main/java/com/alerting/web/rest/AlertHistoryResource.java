ca594b1322ff7fe5edd2b67007d01f91f8d67a65
mirzadevtek <70073170+mirzadevtek@users.noreply.github.com>
2021-06-08 22:44:44 +0800

    alert by project

--- src/main/java/com/alerting/web/rest/AlertHistoryResource.java
+++ src/main/java/com/alerting/web/rest/AlertHistoryResource.java
@@ -16,6 +16,7 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.transaction.annotation.Transactional;
 import org.springframework.web.bind.annotation.*;
 
+import javax.websocket.server.PathParam;
 import java.net.URI;
 import java.net.URISyntaxException;
 
@@ -100,7 +101,29 @@ public class AlertHistoryResource {
     @GetMapping("/alert-histories")
     public List<AlertHistory> getAllAlertHistories() {
         log.debug("REST request to get all AlertHistories");
-        List<AlertHistory> history = alertHistoryRepository.findAll().stream().sorted().filter(b->b.getDateCreated()!=null && (b.getStatus()==null || (b.getStatus()!=null && !b.getStatus().equals("resolved")))).collect(Collectors.toList());
+        List<AlertHistory> history = alertHistoryRepository.findAll().stream()
+                                    .sorted()
+                                    .filter(b->b.getDateCreated()!=null &&
+                                                (b.getStatus()==null ||
+                                                        (b.getStatus()!=null
+                                                                && !b.getStatus().equals("resolved"))))
+                                    .collect(Collectors.toList());
+
+        Collections.sort(history,
+                comparing(AlertHistory::getDateCreated).reversed());
+        return history;
+    }
+
+    @GetMapping("/alert-histories/project/{id}")
+    public List<AlertHistory> getAllAlertHistoriesByProject(@PathVariable Long id) {
+        log.debug("REST request to get all AlertHistories");
+        List<AlertHistory> history = alertHistoryRepository.findAll().stream()
+                .sorted()
+                .filter(b->b.getDateCreated()!=null && b.getTriggeredType().equals("Project") && b.getTriggeredId().equals(id) &&
+                        (b.getStatus()==null ||
+                                (b.getStatus()!=null
+                                        && !b.getStatus().equals("resolved"))))
+                .collect(Collectors.toList());
 
         Collections.sort(history,
                 comparing(AlertHistory::getDateCreated).reversed());
